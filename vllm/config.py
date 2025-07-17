0f72d7c932f957b4f6fe18ea5bf9fd17e65529b2
Thomas Parnell <tpa@zurich.ibm.com>
2025-07-17 00:37:00 -0400

    working chanbges
    Signed-off-by: Thomas Parnell <tpa@zurich.ibm.com>

--- vllm/config.py
+++ vllm/config.py
@@ -4418,6 +4418,7 @@ class CompilationConfig:
                 "vllm.unified_attention_with_output",
                 "vllm.mamba_mixer2",
             ]
+            print("self.splitting_ops: ", self.splitting_ops)
 
 
 @config
