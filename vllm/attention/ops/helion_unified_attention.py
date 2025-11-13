c9a4503e4fcc8985c547031c62ed4466e0d75467
Burkhard Ringlein <ngl@zurich.ibm.com>
2025-11-13 07:41:58 -0500

    fixing stupidity...again
    Signed-off-by: Burkhard Ringlein <ngl@zurich.ibm.com>

--- vllm/attention/ops/helion_unified_attention.py
+++ vllm/attention/ops/helion_unified_attention.py
@@ -20,7 +20,7 @@ def _triton_baseline_fn(
     # k_scale,
     # v_scale,
     t_query_start_lens, # [num_seqs+1]
-    t_query_slots,
+    # t_query_slots,
     # num_seqs,
     # unused, to trigger autotuning...?
     max_seqlen,
@@ -66,7 +66,8 @@ def _triton_baseline_fn(
     # config=helion.Config(block_sizes=[16, 4], 
     #                      indexing=['pointer', 'tensor_descriptor', 'pointer', 'pointer', 'pointer', 'pointer', 'tensor_descriptor', 'pointer', 'pointer'], l2_groupings=[4], load_eviction_policies=['', 'last', 'last', '', '', 'first', 'first', 'first'], loop_orders=[[1, 0, 2], [1, 0]], num_stages=5, num_warps=4, pid_type='flat', range_flattens=[None, True, True, True], range_multi_buffers=[None, False, True, True], range_num_stages=[], range_unroll_factors=[0, 1, 2, 1], range_warp_specializes=[]),
     config=helion.Config(block_sizes=[32, 4], indexing=['pointer', 'pointer', 'pointer', 'pointer', 'tensor_descriptor', 'pointer', 'tensor_descriptor', 'pointer', 'tensor_descriptor'], l2_groupings=[2], load_eviction_policies=['', '', '', '', '', 'last', 'last', ''], loop_orders=[[1, 2, 0], [1, 0]], num_stages=6, num_warps=8, pid_type='flat', range_flattens=[None, True, True, True], range_multi_buffers=[None, None, None, False], range_num_stages=[], range_unroll_factors=[0, 1, 2, 1], range_warp_specializes=[]), 
-    autotune_baseline_fn=_triton_baseline_fn
+    autotune_baseline_fn=_triton_baseline_fn,
+    autotune_effort="quick",
     )
 def kernel_helion_v2_attention(
     t_output,  # [num_tokens, num_query_heads, head_size]
@@ -79,10 +80,10 @@ def kernel_helion_v2_attention(
     # k_scale,
     # v_scale,
     t_query_start_lens, # [num_seqs+1]
-    t_query_slots,
+    # t_query_slots,
     # num_seqs,
     # unused, to trigger autotuning...?
-    max_seqlen,
+    # max_seqlen,
     max_query_len,
     # max_used_querylen_padded: hl.constexpr,
     # is_decode_only: hl.constexpr,
@@ -100,7 +101,13 @@ def kernel_helion_v2_attention(
     num_tokens = t_query.shape[0]
     num_seqs = t_seq_lens.shape[0]
 
+    # TODO: use query_len tensor instead? might safe retuning/recompilation
+    # t_query_lens = torch.diff(t_query_start_lens)
+    # max_query_len = t_query_lens.max()
+    # rather not hl.specialize?
+
     q_block_size = hl.register_block_size(4, int(max_query_len))
+    # q_block_size = hl.register_block_size(4, max_query_len)
     # q_block_size = hl.register_block_size(4, int(max_used_querylen_padded))
     max_qblocks = (max_query_len + q_block_size -1) // q_block_size
 
@@ -119,7 +126,9 @@ def kernel_helion_v2_attention(
         cur_qblock_start = query_start + qblock_idx * q_block_size
         # cur_qblock_end = torch.minimum(query_end, (qblock_idx + 1) * q_block_size)
         # cur_qblock_end = min(query_end, (qblock_idx + 1) * q_block_size)
-        cur_qblock_end = query_start + (qblock_idx + 1) * q_block_size
+        # cur_qblock_end = query_start + (qblock_idx + 1) * q_block_size
+        # cur_qblock_end = torch.maximum(query_start + (qblock_idx + 1) * q_block_size, num_tokens)
+        cur_qblock_end = torch.minimum(query_start + (qblock_idx + 1) * q_block_size, query_end)
 
         # calculating q block index
         # q_block_idxs = torch.where(tile_q.index < query_len, tile_q.index, None)
@@ -136,16 +145,18 @@ def kernel_helion_v2_attention(
             # (tile_q, tile_m, HEAD_SIZE)
             # # tile_q is masked! rather not...
             q = t_query[tile_q, tile_m, :]
+            # q = t_query[0:q_block_size, tile_m, :]
             
-            # # TODO: mask q block
-            q_padding = hl.zeros([1, tile_m, head_size], dtype=q.dtype)  # needs to be creaded with hl. and before torch.where
-            q_condition = (t_query_slots[tile_q] == seq_idx)[:, None, None]
-            # # q = torch.where((tile_q.index < query_len).unsqueeze(1).unsqueeze(2), 
-            # #                 q, 
-            # #                 q_padding)
-            q = torch.where(q_condition,
-                            q, 
-                            q_padding)
+            # # # TODO: mask q block
+            # q_padding = hl.zeros([1, tile_m, head_size], dtype=q.dtype)  # needs to be creaded with hl. and before torch.where
+            # q_condition = (t_query_slots[tile_q] == seq_idx)[:, None, None]
+            # # # q = torch.where((tile_q.index < query_len).unsqueeze(1).unsqueeze(2), 
+            # # #                 q, 
+            # # #                 q_padding)
+            # q = torch.where(q_condition,
+            #                 q, 
+            #                 q_padding)
+            
             # mask q block
             # query_len_in_this_iteration = query_len - tile_q.begin
             # query_len_in_this_iteration = query_len - cur_qblock_start
@@ -172,13 +183,14 @@ def kernel_helion_v2_attention(
             
             # num_blocks = (seq_len + page_size - 1) // page_size 
             # adjust for causal mask
-            max_seq_prefix_len = context_len + tile_q.end + (tile_m.block_size - 1) // num_queries_per_kv + 1
+            max_seq_prefix_len = context_len + tile_q.end + (tile_m.block_size + num_queries_per_kv - 1) // num_queries_per_kv
             max_seq_prefix_len = torch.minimum(max_seq_prefix_len, seq_len)
             num_blocks = torch.ceil(max_seq_prefix_len / page_size)
             for tile_n in hl.tile(num_blocks, block_size=None):
                 block_n_size = tile_n.block_size * page_size
                 # blk_idxs = t_block_tables[seq_idx, tile_n].view([tile_n.block_size * num_par_seq]) #.view(-1)
                 blk_idxs = t_block_tables[seq_idx, tile_n].view([tile_n.block_size]) #.view(-1)
+                # blk_idxs = t_block_tables[seq_idx, 0:tile_n.block_size].view([tile_n.block_size]) #.view(-1)
                 # (tile_n, PAGE_SIZE, 1, HEAD_SIZE)
                 # k = t_key_cache[blk_idxs, :, kv_head, :] # .squeeze(2)
                 k = t_key_cache[blk_idxs, :, kv_head_idx, :]
@@ -227,6 +239,7 @@ def kernel_helion_v2_attention(
             acc = acc / l[:, None]
             # acc = acc / l[:, :, None]
             t_output[tile_q, tile_m, :] = acc.view([tile_q.block_size, tile_m.block_size, head_size])
+            # t_output[0:q_block_size, tile_m, :] = acc.view([tile_q.block_size, tile_m.block_size, head_size])
             # t_output[tile_q, tile_m, :] = acc.view([num_par_seq * tile_q.block_size, tile_m.block_size, head_size])
 
 
@@ -245,7 +258,7 @@ def helion_unified_attention(
     causal,
     window_size,
     block_table,
-    query_slots_mapping,
+    # query_slots_mapping,
     max_query_len_int: int,
     softcap,
     q_descale,
@@ -289,9 +302,9 @@ def helion_unified_attention(
         # v_scale=v_descale,
         t_query_start_lens=cu_seqlens_q,
         # t_query_slots=t_query_slots,
-        t_query_slots=query_slots_mapping,
+        # t_query_slots=query_slots_mapping,
         # num_seqs=num_seqs,
-        max_seqlen=max_seqlen_k,
+        # max_seqlen=max_seqlen_k,
         # max_query_len=max_seqlen_q,
         # max_query_len=int(max_seqlen_q), # need not to be tensor?
         max_query_len=max_query_len_int, # need not to be tensor?
